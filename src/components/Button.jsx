7169be2394443c7e208f4cbdd3e144bff131e62c
Vishal Paswan <vishalvp.3405@gmail.com>
2024-11-21 16:38:03 +0530

    Complete coding part

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,6 @@ export default function Button({
     className='',
     ...props
     }) {
-    
 
     return (
         <>
--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,41 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container";
+import Container from "../components/container/Container";
 import Logo from "./logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-
+import Signup from "./Signup";
+import Login from './Login'
+import PostCard from './PostCard'
+import PostForm from './post-form/PostForm'
+import AuthLayout from './AuthLayout'
+import Button from "./Button";
+import Select from './Select'
+import Input from './Input'
+import AllPosts from '../components/pages/AllPosts'
+import Home from '../components/pages/Home'
+import AddPost from '../components/pages/AddPost'
+import EditPost from '../components/pages/EditPost'
+import Post from '../components/pages/Post'
 export {
     Header,
     Footer,
     Container,
+    EditPost,
     Logo,
     LogoutBtn,
-    RTE
+    AddPost,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Button,
+    Home,
+    AllPosts,
+    Select,
+    Post,
+    Input,
+    
 }
